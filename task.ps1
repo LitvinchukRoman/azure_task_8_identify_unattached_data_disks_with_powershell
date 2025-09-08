fab3f251b0535d0ee3b9fe991db0d27d2cf108e4
litvi <litvin4uk.roman@gmail.com>
2025-09-08 16:25:46 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,10 @@
 # Write your code here
+$AllDisks = Get-AzDisk -ResourceGroupName 'mate-azure-task-5'
+$disks = @()
+foreach ($disk in $AllDisks) {
+    if ($disk.ManagedBy -eq $null) {
+        $disk = $disk | ConvertTo-Json
+        $disks += $disk
+    }
+}
+Out-File -FilePath result.json -InputObject $disks
